b3eec8fbd5287ad565cb281a5946a94e2b8cb288
nndip <nndip@yahoo.com>
2022-05-27 18:37:49 -0400

    update

--- main.tf
+++ main.tf
@@ -58,8 +58,4 @@ output "public_subnets_id" {
   description = "List of IDs of private subnets"
   value       = aws_subnet.public_subnet.*.id
 }
-output "private_subnets_id" {
-  description = "List of IDs of private subnets"
-  value       = aws_subnet.private_subnet.*.id
-}
 
